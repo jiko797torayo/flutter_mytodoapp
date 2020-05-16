db0ba0de861b9c610d6f8dd7aed5e0768cc364f9
jiko797torayo <oec7dwayne7kj@gmail.com>
2020-05-16 16:35:08 +0900

    リスト追加画面作成

--- lib/main.dart
+++ lib/main.dart
@@ -22,12 +22,44 @@ class MyApp extends StatelessWidget {
   }
 }
 
+// リスト一覧画面用Widget
 class TodoListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: Text('リスト一覧画面'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // pushで新規画面に遷移
+          Navigator.of(context).push(
+            MaterialPageRoute(builder: (context) {
+              // 遷移先の画面としてリスト追加画面を指定
+              return TodoAddPage();
+            }),
+          );
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+// リスト追加画面用Widget
+class TodoAddPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: FlatButton(
+          // ボタンをクリックした時の処理
+          onPressed: () {
+            // popで前の画面に戻る
+            Navigator.of(context).pop();
+          },
+          child: Text('リスト追加画面（クリックで戻る）'),
+        ),
       ),
     );
   }
